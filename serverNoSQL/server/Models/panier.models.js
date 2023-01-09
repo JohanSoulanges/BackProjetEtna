2cffc79f5440b25db30e8310724c54f6f5033495
Damiot Johan <damiot_j@etna-alternance.net>
2023-01-09 10:33:52 +0100

    Panier ajout

--- serverNoSQL/server/Models/panier.models.js
+++ serverNoSQL/server/Models/panier.models.js
@@ -3,10 +3,13 @@ const { Schema } = mongoose;
 
 const panierSchema = Schema({
   idUser: { type: String, require: true },
-  products: [{ 
-    productsId: { type: String, require: true },
-    quantité: { type: Number, require: true },
-  }],
+  products: [
+    {
+      productsId: { type: String, require: true },
+      quantity: { type: Number, require: true },
+      price: { type: Number, require: true },
+    },
+  ],
   date: { type: Date, require: true },
   status: { type: Number, require: true },
 });
--- serverNoSQL/server/routes/panier/panier.js
+++ serverNoSQL/server/routes/panier/panier.js
@@ -7,52 +7,64 @@ const router = express.Router();
 
 // Add panier
 router.post("/add", checkToken, function (req, res) {
+  console.log("panier add", req.body);
+
   const token =
     req.headers.authorization &&
     jwt.decode(cleanToken(req.headers.authorization), process.env.SECRET);
-
-  const newPanier = {
-    userId: token.id,
-    productsId: req.body.productsId,
-    quantité: 1,
-    date: new Date.now(),
-  };
 
   Panier.findOne({ userId: token.id }).exec((err, panier) => {
     if (err) {
       console.log("server log :" + err);
     }
     if (panier && panier.status < 2) {
-      panier.forEach((item) => {
-        if (item.productsId === newPanier.productsId) {
-          item.quantité += 1;
-          item.date = new Date.now();
-        }
+      const item = panier.products.find(
+        (item) => item.productsId === req.body.productId
+      );
+      if (item) {
+        item.quantity += 1;
         panier.save();
-        res.json(panier);
+        return res.json(panier);
+      }
+      panier.products.push({
+        productsId: req.body.productId,
+        quantity: 1,
+        price: req.body.price,
       });
-      panier.push({ ...newPanier }, { status: 0 });
       panier.save();
-      res.json(panier);
+      return res.json(panier);
     } else {
-      panier.push({ newPanier }, { status: 0 });
-      panier.save();
-      res.json(newPanier);
+      Panier.create({
+        idUser: token.id,
+        products: [
+          {
+            productsId: req.body.productId,
+            quantity: 1,
+            price: req.body.price,
+          },
+        ],
+        date: Date.now(),
+        status: 0,
+      });
+      return res.json(panier);
     }
   });
 });
 
 router.get("/", checkToken, function (req, res) {
-
   const token =
     req.headers.authorization &&
     jwt.decode(cleanToken(req.headers.authorization), process.env.SECRET);
-    
+
   Panier.findOne({ userId: token.id }).exec((err, panier) => {
     if (err) {
       console.log("server log :" + err);
     }
     res.json(panier);
   });
+});
+
+router.put("/validation", checkToken, function (req, res) {
+  res.json({ message: "panier validation" });
 });
 module.exports = router;
